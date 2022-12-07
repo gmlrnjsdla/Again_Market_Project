b2879042c422c47a1e3837b77358e53937d362b0
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-12-07 15:25:16 +0900

    기본 설정 완료

---
+++ src/test/java/com/hsl/sns/SnsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsl.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

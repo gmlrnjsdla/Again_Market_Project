fec531bdf6ecfdcf77e2cc82f92c48b92487ee8b
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-12-15 11:59:30 +0900

    .

--- src/main/java/com/hsl/sns/controller/HomeController.java
+++ src/main/java/com/hsl/sns/controller/HomeController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
